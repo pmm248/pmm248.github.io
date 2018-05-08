4213f30e38edaef9b02db10b19d637d7be11ada4
Peter Mocarski <pmm248@cornell.edu>
2018-05-08 13:13:51 -0400

    Added chess app

---
+++ js/recommender.js
@@ -0,0 +1,10 @@
+
+
+function getRecommendations(PosAttScore, ClosedOpenScore, GambitsScore, TheoryScore){
+	return [("King", "poop")]
+}
+
+function showResults(){}
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
